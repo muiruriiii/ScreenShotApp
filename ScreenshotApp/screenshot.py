37433554d64bfe0307ff01a01dbcea96880ea551
muiruriiii <muiruricynthiaaa@gmail.com>
2022-11-10 22:13:14 +0300

    Screenshot GUI Final commit

--- ScreenshotApp/screenshot.py
+++ ScreenshotApp/screenshot.py
@@ -1,11 +1,29 @@
 import time
 import pyautogui
+import tkinter as tk
+
 
 def screenshot():
     name = int(round(time.time() * 1000))
     name = 'D:/3rd Year/PythonRealTimeProjects/ScreenshotApp/screenshotsimages/{}.png'.format(name)
-    time.sleep(5)
     img = pyautogui.screenshot(name)
     img.show()
 
-screenshot()
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button (
+    frame,
+    text="Take screenshot",
+    command=screenshot
+)
+button.pack(side=tk.LEFT)
+close = tk.Button(
+    frame,
+    text="Close",
+    command=quit
+)
+close.pack(side=tk.LEFT)
+
+root.mainloop()
